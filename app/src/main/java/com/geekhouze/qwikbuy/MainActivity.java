e34823a8f689d9481ac7450f13cc4aeaa9885fff
geoffie <geoff.seloana@gmail.com>
2018-03-28 20:29:27 +0200

    Added login and register single page design

--- app/src/main/java/com/geekhouze/qwikbuy/MainActivity.java
+++ app/src/main/java/com/geekhouze/qwikbuy/MainActivity.java
@@ -1,15 +1,28 @@
 package com.geekhouze.qwikbuy;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+//                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+//                .setFontAttrId(R.attr.fontPath)
+//                .build()
+//        );
+        setContentView(R.layout.activity_login);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
