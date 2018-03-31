47c68074e619d163b93b8bf8fd4dc287ae061581
geoffie <geoff.seloana@gmail.com>
2018-03-31 17:39:59 +0200

    navigation drawer fix

--- app/src/main/java/com/geekhouze/qwikbuy/MainActivity.java
+++ app/src/main/java/com/geekhouze/qwikbuy/MainActivity.java
@@ -1,6 +1,10 @@
 package com.geekhouze.qwikbuy;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -29,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
+        NavigationView navigationViewDrawer = (NavigationView) findViewById(R.id.navigationViewDrawer) ;
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(navigationViewDrawer);
 
     }
 
@@ -40,5 +46,62 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectItemDrawer(MenuItem menuItem){
+        Fragment mFragment = null;
+        Class mFragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.dashboard:
+                mFragmentClass = Home.class;
+                break;
+            case R.id.mostSelling:
+                mFragmentClass = MostSelling.class;
+                break;
+            case R.id.dailyDeals:
+                mFragmentClass = DailyDeals.class;
+                break;
+            case R.id.sale:
+                mFragmentClass = Sale.class;
+                break;
+            case R.id.Orders:
+                mFragmentClass = Orders.class;
+                break;
+            case R.id.wishList:
+                mFragmentClass = WishList.class;
+                break;
+            case R.id.settings:
+                mFragmentClass = Settings.class;
+                break;
+            case R.id.logout:
+                mFragmentClass = Home.class;
+                break;
+            default:
+                mFragmentClass = Home.class;
+                break;
+        }
+
+        try {
+            mFragment = (Fragment) mFragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, mFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true ;
+            }
+        });
     }
 }
