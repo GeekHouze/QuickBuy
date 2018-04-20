e918eac8853595b1deda4a6c53606768cc8c15ee
geoffie <geoff.seloana@gmail.com>
2018-04-20 18:44:52 +0200

    fixed padding

--- app/src/main/java/com/geekhouze/qwikbuy/WishList.java
+++ app/src/main/java/com/geekhouze/qwikbuy/WishList.java
@@ -24,6 +24,7 @@ public class WishList extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
