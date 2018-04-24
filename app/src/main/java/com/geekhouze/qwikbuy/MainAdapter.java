983b6ab075189f95953ada0372499c3ceba5350a
geoffie <geoff.seloana@gmail.com>
2018-04-24 19:49:54 +0200

    Image Slider fix

--- app/src/main/java/com/geekhouze/qwikbuy/MainAdapter.java
+++ app/src/main/java/com/geekhouze/qwikbuy/MainAdapter.java
@@ -15,7 +15,17 @@ class  MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
 
     private ArrayList<String> mDataset;
 
+    public MainAdapter(ArrayList<String> mDataset) {
+        this.mDataset = mDataset;
+    }
 
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
 
     @Override
     public int getItemCount() {
@@ -42,8 +52,6 @@ class  MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
 
         }
     }
-
-
 
     @Override
     public void onBindViewHolder(MainAdapter.ViewHolder holder, int position) {
