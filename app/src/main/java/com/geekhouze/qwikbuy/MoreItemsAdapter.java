27b3d35b52a7faa00137c42390a2201a43afc163
geoffie <geoff.seloana@gmail.com>
2018-04-26 22:53:14 +0200

    Image Slider fix

--- app/src/main/java/com/geekhouze/qwikbuy/MoreItemsAdapter.java
+++ app/src/main/java/com/geekhouze/qwikbuy/MoreItemsAdapter.java
@@ -52,7 +52,10 @@ public class MoreItemsAdapter extends RecyclerView.Adapter<MoreItemsAdapter.MyVi
     }
 
 
-
+    public MoreItemsAdapter(Context mContext, List<Album> albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
