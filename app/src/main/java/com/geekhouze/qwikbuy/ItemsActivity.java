f4493bed4ab9b44c52ab3e5e1b5ed4d001e66829
geoffie <geoff.seloana@gmail.com>
2018-04-28 12:32:46 +0200

    Image Slider fix

--- app/src/main/java/com/geekhouze/qwikbuy/ItemsActivity.java
+++ app/src/main/java/com/geekhouze/qwikbuy/ItemsActivity.java
@@ -2,13 +2,37 @@ package com.geekhouze.qwikbuy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.geekhouze.qwikbuy.cardview.Album;
+import com.synnapps.carouselview.CarouselView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView2;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.Adapter mAdapter2;
+    private RecyclerView.Adapter mAdapter3;
+    private ArrayList<String> mDataset;
+    private CarouselView carouselView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        mRecyclerView2 = (RecyclerView) findViewById(R.id.recycler_view2);
+        mRecyclerView2.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView2.setLayoutManager(mLayoutManager);
+        mAdapter2 = new MainAdapter(mDataset);
+        mRecyclerView2.setAdapter(mAdapter2);
 
     }
 }
