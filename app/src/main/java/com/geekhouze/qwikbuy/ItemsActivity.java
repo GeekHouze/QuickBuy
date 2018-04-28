21fcc11f10c2b65fd8b694cddc504afaa08359cc
geoffie <geoff.seloana@gmail.com>
2018-04-28 16:19:45 +0200

    Image Slider fix

--- app/src/main/java/com/geekhouze/qwikbuy/ItemsActivity.java
+++ app/src/main/java/com/geekhouze/qwikbuy/ItemsActivity.java
@@ -1,9 +1,14 @@
 package com.geekhouze.qwikbuy;
 
+import android.graphics.Outline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.geekhouze.qwikbuy.cardview.Album;
 import com.synnapps.carouselview.CarouselView;
@@ -22,10 +27,33 @@ public class ItemsActivity extends AppCompatActivity {
     private ArrayList<String> mDataset;
     private CarouselView carouselView;
 
+    ImageButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        fab = (ImageButton) findViewById(R.id.fab);
+
+        //Outline
+        //int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        //Outline outline = new Outline();
+        //outline.setOval(0, 0, size, size);
+        //findViewById(R.id.fab).setOutline(outline);
+
+        ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                // Or read size directly from the view's width/height
+                int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
+                outline.setOval(0, 0, size, size);
+            }
+        };
+
+        fab.setOutlineProvider(viewOutlineProvider);
+
+
 
         mDataset = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
