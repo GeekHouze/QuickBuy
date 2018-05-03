da8a7db6555467647a08ba9da4328b8df118d754
geoffie <geoff.seloana@gmail.com>
2018-05-03 17:24:23 +0200

    added item-selected page

--- app/src/main/java/com/geekhouze/qwikbuy/Home.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Home.java
@@ -89,14 +89,7 @@ public class Home extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
 
     Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
