d329ed70a723cd178d2d438ddef2a92dd0bf1bd5
geoffie <geoff.seloana@gmail.com>
2018-05-03 17:25:48 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Help.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Help.java
@@ -35,20 +35,6 @@ public class Help extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-
-
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-    }
 
 
 
