36e49f4dcac44b4c239b753090f7e4750ed1e168
geoff <geoff.seloana@gmail.com>
2018-05-23 21:01:09 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Help.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Help.java
@@ -26,29 +26,7 @@ public class Help extends Fragment {
 
     }
 
-    public static Help newInstance(String param1, String param2) {
-        Help fragment = new Help();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-
-
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-    }
 
 
 
