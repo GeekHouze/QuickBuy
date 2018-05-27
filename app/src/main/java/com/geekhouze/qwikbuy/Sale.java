e8cec4ff01cc4872a13e693abf1c10e6476cbcba
geoff <geoff.seloana@gmail.com>
2018-05-27 12:36:18 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Sale.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Sale.java
@@ -42,7 +42,14 @@ public class Sale extends Fragment {
      * @return A new instance of fragment Sale.
      */
     // TODO: Rename and change types and number of parameters
-
+    public static Sale newInstance(String param1, String param2) {
+        Sale fragment = new Sale();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
