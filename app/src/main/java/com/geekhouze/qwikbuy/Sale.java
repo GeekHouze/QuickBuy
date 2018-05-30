5d99db33c40a2e4b0d25d70c30b24ef92b0848ab
geoff <geoff.seloana@gmail.com>
2018-05-30 17:22:42 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Sale.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Sale.java
@@ -60,6 +60,12 @@ public class Sale extends Fragment {
         }
     }
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
