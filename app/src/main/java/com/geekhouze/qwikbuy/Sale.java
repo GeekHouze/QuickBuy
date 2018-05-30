9947a27c079e60b9f01ca82df971b293f9354df7
geoff <geoff.seloana@gmail.com>
2018-05-30 17:21:21 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Sale.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Sale.java
@@ -75,6 +75,11 @@ public class Sale extends Fragment {
     }
 
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
     /**
      * This interface must be implemented by activities that contain this
