186865a674cca7a7acee15726357c819739dbaf6
geoff <geoff.seloana@gmail.com>
2018-06-01 22:26:19 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Sale.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Sale.java
@@ -52,12 +52,7 @@ public class Sale extends Fragment {
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sale, container, false);
-    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
