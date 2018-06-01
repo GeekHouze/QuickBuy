61e649a621cc360707b837f4157753a5636d40c3
geoff <geoff.seloana@gmail.com>
2018-06-01 22:26:49 +0200

    New feature

--- app/src/main/java/com/geekhouze/qwikbuy/Sale.java
+++ app/src/main/java/com/geekhouze/qwikbuy/Sale.java
@@ -55,11 +55,6 @@ public class Sale extends Fragment {
 
 
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-    }
 
     @Override
     public void onDetach() {
