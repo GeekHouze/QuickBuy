29d21203a877c03ba7fe2e8fe91b384f13c9545e
geoff <geoff.seloana@gmail.com>
2018-06-05 22:41:15 +0200

    New feature

---
+++ app/src/main/java/com/geekhouze/qwikbuy/ItemsActivity.java
@@ -0,0 +1,47 @@
+package com.geekhouze.qwikbuy;
+
+import android.graphics.Outline;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.geekhouze.qwikbuy.cardview.Album;
+import com.synnapps.carouselview.CarouselView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView2;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.Adapter mAdapter2;
+    private RecyclerView.Adapter mAdapter3;
+    private ArrayList<String> mDataset;
+    private CarouselView carouselView;
+
+
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id =item.getItemId();
+
+        if(id == android.R.id.home){
+            //
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
