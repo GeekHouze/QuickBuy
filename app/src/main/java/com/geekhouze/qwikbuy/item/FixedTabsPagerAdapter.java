0fec90eb7109140c0b0b50d85c6f9e41ddc59ea5
geoff <geoff.seloana@gmail.com>
2018-06-09 17:44:09 +0200

    New feature tabs

---
+++ app/src/main/java/com/geekhouze/qwikbuy/item/FixedTabsPagerAdapter.java
@@ -0,0 +1,62 @@
+package com.geekhouze.qwikbuy.item;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.geekhouze.qwikbuy.R;
+import com.google.auto.value.extension.AutoValueExtension;
+
+import java.util.ResourceBundle;
+
+public class FixedTabsPagerAdapter extends FragmentPagerAdapter{
+
+    private Context context;
+
+    public FixedTabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                // return Fragment
+                return null;
+            case 1:
+                // return Fragment
+                return null;
+            case 2:
+                // return Fragment
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position){
+            case 0:
+                return  getString(R.string.description);
+            case 1:
+                return getString(R.string.info);
+            case 2:
+                return getString(R.string.reviews);
+            default:
+                return null;
+
+        }
+    }
+
+    private CharSequence getString(int id) {
+        return context.getResources().getString(id);
+    }
+}
